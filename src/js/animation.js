b86945d74059d4e1c6685738adc10490a53807b3
Claire <claire.me.0501@gmail.com>
2018-02-03 22:46:58 +0800

    complete notifications animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,2 +1,25 @@
-// import {initButton} from 'header'
 import '../styles/animation.sass';
+
+const $ = require("jquery");
+
+$(function () {
+
+  $('.section-notification .nav li').click(function(){
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+
+    if ($(this).hasClass('search')) {
+      $('.section-notification .alert-element').addClass('is-active');
+      setTimeout(function() {
+        $('.section-notification .alert-element').removeClass('is-active');
+      }, 3000);
+    }
+
+    if ($(this).hasClass('face')) {
+      $('.section-notification .overlay').addClass('is-active');
+      setTimeout(function(){
+        $('.section-notification .overlay').removeClass('is-active');
+      }, 3000);
+    }
+  });
+
+});
